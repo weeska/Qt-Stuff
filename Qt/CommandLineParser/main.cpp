7793311811fc8e5462b9bd3fbb11bf80d27afac7
weeska <weeska@gmail.com>
2015-05-16 22:46:31 +0200

    Added option and positional argument to CommandLineParser example

--- Qt/CommandLineParser/main.cpp
+++ Qt/CommandLineParser/main.cpp
@@ -1,5 +1,10 @@
+#include <cstdio>
+#include <ctime>
+#include <limits>
+
 #include <QCoreApplication>
 #include <QCommandLineParser>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
@@ -14,7 +19,26 @@ int main(int argc, char *argv[])
     parser.addVersionOption();
     parser.addHelpOption();
 
+    QCommandLineOption randomNumberOption(QStringList() << "r" << "random", "Writes a random number to stdout.");
+    parser.addOption(randomNumberOption);
+
+    parser.addPositionalArgument("min", "Minimal random value.");
+    parser.addPositionalArgument("max", "Maximal random value.");
+
     parser.process(argumments);
+
+    if(parser.isSet(randomNumberOption))
+    {
+        srand(time(0));
+        const QStringList &positionalArguments = parser.positionalArguments();
+
+        const int min = (positionalArguments.size() > 0 ?
+                             positionalArguments[0].toInt() : 0);
+        const int max = (positionalArguments.size() > 1 ?
+                             positionalArguments[1].toInt() : std::numeric_limits<int>::max());
+
+        QTextStream ( stdout ) << rand() % (max - min + 1) + min;
+    }
 
     return a.exec();
 }
